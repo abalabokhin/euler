83a0d5c0b2084a5e73a9f123228d39303291a0e4
Andrey Balabokhin <abalabokhin@gmail.com>
2016-07-24 15:35:46 -0400

    first commit

---
+++ p2/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut n1 = 1;
+    let mut n2 = 2;
+    let mut sum = 0i64;
+    while n2 < 4000000 {
+        if n2 % 2 == 0 {
+            sum += n2;
+        }
+        let (t1, t2) = (n2, n1 + n2); 
+        n1 = t1;
+        n2 = t2;
+    }
+    println!("{}", sum);
+}
---
+++ p3/src/main.rs
@@ -0,0 +1,14 @@
+use std::f64;
+
+fn main() {
+    let mut n = 600851475143i64;
+    for x in 2..n {
+        println!("");
+    
+    }
+
+
+
+
+}
+
